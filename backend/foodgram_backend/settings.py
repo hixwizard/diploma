53958f05d57fd8a8afb6f90858c3ba91227c4294
hix9 <stanislavbarinov0@gmail.com>
2024-09-18 21:32:15 +0300

    ngingx

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -105,13 +105,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-
+STATIC_URL = '/bstatic/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/bmedia/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
