faee985dde0cb7eb13eed24b081f85942ad44f56
hix9 <stanislavbarinov0@gmail.com>
2024-09-19 13:14:33 +0300

    fix DIR to static, media

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -108,13 +108,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = STATIC_ROOT = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = MEDIA_ROOT = '/media/'
 
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
